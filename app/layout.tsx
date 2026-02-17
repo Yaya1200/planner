cb762cf61cbf1e45ac51ab1c9ec5ff81c2359824
yaredgebre <yaredgebre120@gmail.com>
2026-02-17 14:45:42 -0300

    adding header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+import "./globals.css"
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      home
+    <html lang="eng">
+      <body>
+        <Header/>
+        {children}
+        <Footer/>
+        
+      </body>
     </html>
   );
 }
