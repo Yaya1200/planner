41e660ecb47e2ea81377f86d2817ec5506f783fa
yaredgebre <yaredgebre120@gmail.com>
2026-02-17 14:46:25 -0300

    adding the header

---
+++ app/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div><header >Planner</header>
+      <input placeholder="what to search"/>
+      <button>search</button></div>
+  )
+}
+
+export default Header
