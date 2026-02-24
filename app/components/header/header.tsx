c715c0beabf56dca7e70d257a74f6012e5951895
yaredgebre <yaredgebre120@gmail.com>
2026-02-24 07:10:12 -0300

    adding header logo

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Header() {
   return (
-    <div><header >Planner</header>
+    <div>
+      <div className='planner-logo'></div>
+      <p>Planner</p>
       <input placeholder="what to search"/>
       <button>search</button></div>
   )
