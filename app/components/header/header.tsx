e5b637c5a2bb284b2b148d189c6664db8c86db60
yaredgebre <yaredgebre120@gmail.com>
2026-02-26 16:59:00 -0300

    updating the font size

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -6,7 +6,7 @@ function Header() {
       <div className='logo-container'>
         <div className='planner-logo'></div>
       <div className='planner-style'>Planner</div>
-      <input className='planner-style' placeholder="what to search"/>
+      <input  placeholder="what to search"/>
       <button>search</button></div>
       </div>
       
