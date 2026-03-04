d5841ef6ae7e1bbe11b528b01b2e3f7ae90b610d
yaredgebre <yaredgebre120@gmail.com>
2026-03-04 15:12:38 -0300

    adding links

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
+    <div >
+      <div style={{display:'flex', gap:"20px", justifyContent:"flex-end", margin:"10px"}}>
+      <a href='/' className='header-link'>Home</a>
+      <a href='viewNotes' className='header-link'>Note</a>
+      </div>
     <div className='header-container'>
       <div className='logo-container'>
         <div className='planner-logo'></div>
@@ -10,6 +15,7 @@ function Header() {
        <div className='search-button'>
         <input  placeholder="what to search"/>
       <button>search</button>
+      </div>
       </div>
       </div>
       
