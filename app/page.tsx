5d5480f14037b0b0dba53818e9b405b494baacca
yaredgebre <yaredgebre120@gmail.com>
2026-03-07 17:07:33 -0300

    adding the add note in the page

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ export default function Home() {
        <span className="continer-name"> Backlog</span>  <br/>
         <button>+</button> <span className="add-name"> add a card</span>
       </div>
-      <div>
+      <div className="container-middle">
         <button>Backlog</button>
         <button>In Progress</button>
         <button>Done</button>
       </div>
-      <div className="in-progress" style={{ display: "not-none" }}>
+      <div className="in-progress" style={{ display: "none" }}>
         <span className="continer-name"> In Progress</span>  <br/>
         <button>+</button> <span className="add-name"> add a card</span>
       </div>
