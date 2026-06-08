1c7ec93263b02cfe3fbe592f019cd9be7f032cd4
Yared Gebre <yaredgebre120@gmail.com>
2026-06-08 15:25:07 -0300

    Fix formatting issues in footer.tsx

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from 'react';
 
 function Footer() {
   return (
     <div className="footer">&copy; 2026 planner app  made with passion</div>
   )
 }
-
-export default Footer
+export default Footer;
