94e56384f5c8c299d12688728c868ea69200c79f
yaredgebre <yaredgebre120@gmail.com>
2026-07-03 13:46:41 -0300

    adding header

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
+import Link from "next/link";
 
 function Header() {
   return (
-    <div >
-      <div style={{display:'flex', gap:"20px", justifyContent:"flex-end", margin:"10px"}}>
-      <a href='/' className='header-link'>Home</a>
-      <a href='viewNotes' className='header-link'>Note</a>
+    <header className="site-header">
+      <div className="header-top">
+        <Link href="/" className="header-link">
+          Home
+        </Link>
+        <Link href="/viewNotes" className="header-link">
+          Notes
+        </Link>
       </div>
-    <div className='header-container'>
-      <div className='logo-container'>
-        <div className='planner-logo'></div>
-      <div className='planner-style'>Planner</div>
+
+      <div className="header-container">
+        <div className="logo-container">
+          <div className="planner-logo" />
+          <div>
+            <div className="planner-style">Planner</div>
+            <p className="header-subtitle">Keep your work moving</p>
+          </div>
+        </div>
       </div>
-       <div className='search-button'>
-        <input  placeholder="what to search"/>
-      <button>search</button>
-      </div>
-      </div>
-      </div>
-      
-      
-  )
+    </header>
+  );
 }
 
 export default Header
