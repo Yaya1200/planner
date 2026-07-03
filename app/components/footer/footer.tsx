041e4cf6b84c3d052a4b5ecd65129c7496d7072a
yaredgebre <yaredgebre120@gmail.com>
2026-07-03 13:51:16 -0300

    updating footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 function Footer() {
   return <footer className="footer">&copy; 2026 Planner App. Built for focused collaboration.</footer>;
-=======
-import React from 'react';
-
-function Footer() {
-  return (
-    <div className="footer">&copy; 2026 planner app  made with passion</div>
-  )
->>>>>>> c4452ab05f5cfc6162c6703c0269fbcb2507176c
 }
+
 export default Footer;
